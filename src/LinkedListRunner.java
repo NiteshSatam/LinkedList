a192f47b5b4be832648476d7de276b90e9bc542a
NiteshSatam <niteshsatam55@gmail.com>
2022-06-26 10:42:58 +0530

    Added: Simple Linked List

--- src/LinkedListRunner.java
+++ src/LinkedListRunner.java
@@ -4,6 +4,8 @@ public class LinkedListRunner {
 	public static void main(String[] args) {
 		LinkedList linkedList = new LinkedList();
 
+		linkedList.addFirst(70);
+		linkedList.addFirst(30);
 		linkedList.addFirst(56);
 		linkedList.display();
 
