c78566ed7df9e88b6441a08b4b8da8d188b3fed9
NiteshSatam <niteshsatam55@gmail.com>
2022-06-26 11:01:24 +0530

    Added: Add last logic

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -26,6 +26,24 @@ public class LinkedList {
 
 	}
 
+	public void addLast(Object data) {
+		Node newNode = new Node(data);
+
+		if (head == null)
+			head = newNode; // head=>[1 |null ] // list is notempty
+		else {
+
+			Node temp = head;
+
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+
+			temp.next = newNode;
+		}
+
+	}
+
 	public void display() {
 
 		if (head == null) {
